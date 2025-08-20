54d4135fa43a3f1c0a4dd2563176659634ffa308
Gustavo <matsuogustavo04@gmail.com>
2025-08-20 09:55:21 -0300

    acho
    coisa

---
+++ src/estoque/Produto.java
@@ -0,0 +1,48 @@
+package estoque;
+
+public class Produto {
+	String nome;
+	int codigo;
+	double preco;
+	int quantidade;
+	
+	public Produto(String nome, int codigo, double preco, int quantidade) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
